73d01796e377711d135e737bcfa47efa1c727445
javakidx <javakidx@gmail.com>
2015-11-12 01:26:57 +0800

    initial commit

---
+++ java-is-fun/src/main/java/idv/jk/algorithm/enumeration/SimpleMatchesEquation.java
@@ -0,0 +1,12 @@
+package idv.jk.algorithm.enumeration;
+
+/**
+ * Created by bioyang on 2015/11/12.
+ */
+public class SimpleMatchesEquation
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
