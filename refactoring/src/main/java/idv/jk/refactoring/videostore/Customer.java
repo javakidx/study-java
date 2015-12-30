750b4224f1d48563f116ddae00f4655cc98c2a8c
javakidx <javakidx@gmail.com>
2015-12-30 17:56:46 +0800

    Customer class

--- refactoring/src/main/java/idv/jk/refactoring/videostore/Customer.java
+++ refactoring/src/main/java/idv/jk/refactoring/videostore/Customer.java
@@ -1,5 +1,21 @@
 package idv.jk.refactoring.videostore;
 
-public class Customer {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Customer {
+	private String name;
+	private List<Rental> rentalList = new ArrayList<>();
+	
+	public Customer(String name){
+		this.name = name;
+	}
+	
+	public void addRental(Rental rental) {
+		this.rentalList.add(rental);
+	}
+	
+	public String getName() {
+		return this.name;
+	}
 }
