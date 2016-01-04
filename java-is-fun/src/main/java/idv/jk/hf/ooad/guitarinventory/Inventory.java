cc07ab48009c58ebd931ee22a5c4915ea43b23b7
javakidx <javakidx@gmail.com>
2016-01-04 23:49:23 +0800

    add Inventory class

---
+++ java-is-fun/src/main/java/idv/jk/hf/ooad/guitarinventory/Inventory.java
@@ -0,0 +1,23 @@
+package idv.jk.hf.ooad.guitarinventory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by bioyang on 2016/1/4.
+ */
+public class Inventory {
+    private List<Guitar> guitarList = new ArrayList<>();
+
+    public void addGuitar(String serialNumber, double price, GuitarSpec spec) {
+
+    }
+
+    public Guitar getGuitar(String serialNumber) {
+        return null;
+    }
+
+    public List<Guitar> search(GuitarSpec spce) {
+        return null;
+    }
+}
