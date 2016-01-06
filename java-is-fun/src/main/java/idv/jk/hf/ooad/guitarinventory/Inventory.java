ee2ab5d877777fd143384e478bb507df3576d23c
javakidx <javakidx@gmail.com>
2016-01-07 00:22:16 +0800

    new search method

--- java-is-fun/src/main/java/idv/jk/hf/ooad/guitarinventory/Inventory.java
+++ java-is-fun/src/main/java/idv/jk/hf/ooad/guitarinventory/Inventory.java
@@ -10,18 +10,47 @@ public class Inventory {
     private List<Instrument> instrumentList = new ArrayList<>();
 
     public void addInstrument(String serialNumber, double price, InstrumentSpec spec) {
+        Instrument instrument = null;
 
+        if (spec instanceof GuitarSpec) {
+            instrument = new Guitar(serialNumber, price, (GuitarSpec)spec);
+        } else if (spec instanceof MandolinSpec) {
+            instrument = new Mandolin(serialNumber, price, (MandolinSpec)spec);
+        }
+
+        instrumentList.add(instrument);
     }
 
-    public Guitar get(String serialNumber) {
+    public Instrument get(String serialNumber) {
+        for (Instrument instrument : this.instrumentList) {
+            if (instrument.getSerialNumber().equals(serialNumber)) {
+                return instrument;
+            }
+        }
         return null;
     }
 
-    public List<Guitar> search(GuitarSpec spce) {
+    public Guitar getGuitar(String serialNumber) {
+        for (Instrument instrument : this.instrumentList) {
+            Guitar guitar = (Guitar)instrument;
+            if (guitar.getSerialNumber().equals(serialNumber)) {
+                return guitar;
+            }
+        }
         return null;
     }
 
     public List<Mandolin> search(MandolinSpec spec) {
-        return null;
+        List<Mandolin> matchingMandolins = new ArrayList<>();
+
+        for (Instrument instrument : this.instrumentList) {
+            Mandolin mandolin = (Mandolin)instrument;
+
+            if (mandolin.getSpec().matches(spec)) {
+                matchingMandolins.add(mandolin);
+            }
+        }
+
+        return matchingMandolins;
     }
 }
